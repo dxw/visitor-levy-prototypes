514e4d1561f0e9ae0c343530bbcb68f01de68e9b
Harry Trimble <harryroberttrimble@gmail.com>
2024-02-15 14:34:59 +0000

    Improve registration journey

--- app/routes.js
+++ app/routes.js
@@ -11,3 +11,13 @@ const router = govukPrototypeKit.requests.setupRouter()
 router.get("/", function (request, response) {
   response.redirect('/en/index')
 });
+
+router.post('/en/register/postcode', function(request, response) {
+
+    var inputtype = request.session.data['inputtype']
+    if (inputtype == "spreadsheet"){
+        response.redirect("/en/register/upload-spreadsheet")
+    } else {
+        response.redirect("/en/register/postcode")
+    }
+})
